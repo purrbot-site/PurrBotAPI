7856d194148eae3396bbc7698b5d0460078b9f66
Andre601 <11576465+Andre601@users.noreply.github.com>
2020-04-05 02:18:44 +0200

    Remove deprecated fields

--- src/main/java/site/purrbot/api/endpoints/Quote.java
+++ src/main/java/site/purrbot/api/endpoints/Quote.java
@@ -1,19 +1,12 @@
 package site.purrbot.api.endpoints;
 
-import com.google.gson.annotations.SerializedName;
-
 public class Quote{
     
     private String avatar = "https://i.imgur.com/63aniDJ.png";
-    @SerializedName(value = "nameColor", alternate = {"color"})
     private String nameColor = "hex:ffffff";
-    @SerializedName(value = "dateFormat", alternate = {"format"})
     private String dateFormat = "dd. MMM yyyy hh:mm:ss zzz";
-    @SerializedName(value = "username", alternate = {"name"})
     private String username = "Someone";
-    @SerializedName(value = "message", alternate = {"text"})
     private String message = "Some message.";
-    @SerializedName(value = "timestamp", alternate = {"time"})
     private String timestamp = String.valueOf(System.currentTimeMillis());
     
     public String getAvatar(){
